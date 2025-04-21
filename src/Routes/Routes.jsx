272483222572eeaa4c9d9f3efc9efc6ff9fcac9a
Akash <hafejmohammadakash@gmail.com>
2025-04-21 12:15:02 +0600

    details card banalam

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,43 +1,49 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../Layouts/MainLayouts";
-import Home from "../Pages/Home"
-import Coffees from "../Pages/Coffees"
-import Dashboard from "../Pages/Dashboard"
+import Home from "../Pages/Home";
+import Coffees from "../Pages/Coffees";
+import Dashboard from "../Pages/Dashboard";
 import CoffeesCard from "../Pages/CoffeesCard";
+import CoffeeDetails from "../Pages/CoffeeDetails";
 
 const routes = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainLayouts />,
+    children: [
+      {
         path: "/",
-        element: <MainLayouts />,
+        element: <Home />,
+        loader: () => fetch("../categories.json"),
         children: [
-            {
-                path: "/",
-                element: <Home />, 
-                loader: () => fetch("../categories.json"),
-                children: [
-                    {   path: "/",
-                        element: <CoffeesCard />,
-                        loader: () => fetch("../coffees.json")
-                    },
-                    {   path: "/category/:category",
-                        element: <CoffeesCard />,
-                        loader: () => fetch("../coffees.json")
-                    },
-
-                ]
-            },
-            {
-                path: "/coffees",
-                element: <Coffees /> ,
-                loader: () => fetch("../coffees.json")
-            },
-            {
-                path: "/dashboard",
-                element: <Dashboard /> 
-            },
-
-        ]
-    }
+          {
+            path: "/",
+            element: <CoffeesCard />,
+            loader: () => fetch("../coffees.json"),
+          },
+          {
+            path: "/category/:category",
+            element: <CoffeesCard />,
+            loader: () => fetch("../coffees.json"),
+          },
+        ],
+      },
+      {
+        path: "/coffees",
+        element: <Coffees />,
+        loader: () => fetch("../coffees.json"),
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/coffee/:id",
+        element: <CoffeeDetails />,
+        loader: () => fetch("../coffees.json"),
+      },
+    ],
+  },
 ]);
 
 export default routes;
