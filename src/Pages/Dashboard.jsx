3e349f451cb501b01849d6c881747f3281d959b9
Akash <hafejmohammadakash@gmail.com>
2025-04-22 03:08:23 +0600

    dashboard er kaj kaorlam data niye gela localstorage

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getCoffeeCart } from "../utils/LocalStorage";
+import Card from "./Card";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h2>Dasboard</h2>
-        </div>
-    );
+  const [coffees, setCoffee] = useState([]);
+
+  useEffect(() => {
+    const favorite = getCoffeeCart();
+    setCoffee(favorite);
+  }, []);
+
+  return (
+    <div>
+      <h2>Dasboard</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-6">
+        {coffees.map((coffee) => (
+          <Card key={coffee.id} coffee={coffee} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
